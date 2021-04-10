3297de769a95a7fa48d00fd74e4fcc5515dc61b3
dock6699 <82054005+dock6699@users.noreply.github.com>
2021-04-10 17:07:47 +0300

    a

---
+++ Assets/Scripts/DestroyArea.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyArea : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.GetComponent<EnemyBehavior>())
+        {
+            Destroy(collision.gameObject);
+        }
+     
+    }
+}
---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    [SerializeField] private float _spawnCooldown;
+    [SerializeField] private GameObject _objectForSpawn;
+
+    private Transform[] _spawnPoints;
+
+    private void Start()
+    {
+        _spawnPoints = new Transform[gameObject.transform.childCount];
+
+        for (int i = 0; i < _spawnPoints.Length; i++)
+        {
+            _spawnPoints[i] = gameObject.transform.GetChild(i);
+        }
+
+        StartCoroutine(SpawnCooldown());
+    }
+
+    private IEnumerator SpawnCooldown()
+    {
+        var waitForSeconds = new WaitForSeconds(_spawnCooldown);
+
+        for (int i = 0; i < _spawnPoints.Length;i++)
+        {
+            Instantiate(_objectForSpawn, _spawnPoints[i].position, Quaternion.identity);
+
+            if (i >= _spawnPoints.Length-1)
+            {
+                i = -1;
+            }
+
+            yield return waitForSeconds;
+        }
+        
+    }
+}
